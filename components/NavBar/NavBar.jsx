c2b61b1f479aab0cd14908c68ebf95b9b1532320
anastasiiachikrizova <ana@upword.ai>
2023-02-06 21:01:41 +0200

    1

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -3,33 +3,50 @@ import Image from "next/image";
 import styles from "./NavBar.module.css";
 
 export default function NavBar() {
+  function start() {
+    gapi.load("auth2", function () {
+      auth2 = gapi.auth2.init({
+        client_id: "878280196612-1m8913mbl7b95pvvm701j486k0r6ftfi.apps.googleusercontent.com",
+        // Scopes to request in addition to 'profile' and 'email'
+        //scope: 'additional_scope'
+      });
+    });
+  }
+  const clicks = () => {
+    // signInCallback defined in step 6.
+    auth2.grantOfflineAccess().then(signInCallback);
+  };
   return (
-    <div className={styles.container}>
-      <div className={styles.logo}>
-        {" "}
-        <Image src="/logo.svg" width={105} height={105} />
+    <>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <button id="signinButton" onClick={clicks}>
+            Sign in with Google
+          </button>
+          <Image src="/logo.svg" width={105} height={105} />
+        </div>
+
+        <nav className={styles.nav}>
+          <ul className={styles.nav__list}>
+            <li className={styles.nav__item}>
+              <Link href="/">[ Главная ] </Link>
+            </li>
+
+            <li className={styles.nav__item}>
+              <Link href="/About">О страйкболе</Link>
+            </li>
+            <li className={styles.nav__item}>
+              <Link href="/Rules">Правила</Link>
+            </li>
+            <li className={styles.nav__item}>
+              <Link href="/Prices">Цены</Link>
+            </li>
+            <li className={styles.nav__item}>
+              <Link href="/Contacts">Контакты</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-
-      <nav className={styles.nav}>
-        <ul className={styles.nav__list}>
-          <li className={styles.nav__item}>
-            <Link href="/">[ Главная ] </Link>
-          </li>
-
-          <li className={styles.nav__item}>
-            <Link href="/About">О страйкболе</Link>
-          </li>
-          <li className={styles.nav__item}>
-            <Link href="/Rules">Правила</Link>
-          </li>
-          <li className={styles.nav__item}>
-            <Link href="/Prices">Цены</Link>
-          </li>
-          <li className={styles.nav__item}>
-            <Link href="/Contacts">Контакты</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    </>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" async></script>
+        <script
+          src="https://apis.google.com/js/client:platform.js?onload=start"
+          async
+          defer
+        ></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
